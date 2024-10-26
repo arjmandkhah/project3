f6a34b47304a72eb2274388cb49a0e700a7a089d
arjmandkhah <arjmand9551@gmail.com>
2024-10-26 23:38:33 +0330

    Create _home.js

---
+++ js/_home.js
@@ -0,0 +1,5 @@
+// $("div.main-card").css({
+//     backgroundImage:url("C:\web\project3\img\pexels-itsterrymag-2635038"),
+// })
+// let gh=document.querySelector("div.main-card")
+// gh.style.backgroundImsge="url('C:\web\project3\img\pexels-itsterrymag-2635038')";
