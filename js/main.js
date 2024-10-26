4a9c0e8aa5851279879f417cab27c089dd3ef19b
arjmandkhah <arjmand9551@gmail.com>
2024-10-26 23:38:45 +0330

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// // import"../js/_home";
+// import "./_cotact";
+// $("div#content").css({
+//     backgroundColor:"blue",
+//     width:"340px",
+//     height:"300px",
+// })
+// let r =document.querySelector("div.fgh")
+// r.style.backgroundColor="blue";
+// r.style.height="90px";
+// // r.style.width="350px";
